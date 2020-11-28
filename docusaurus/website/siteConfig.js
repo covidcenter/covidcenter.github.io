38e2796e68a9c65de6644d69a6050893deeb9b23
gazandi <gazandi@ruangguru.com>
2020-11-28 10:40:20 +0700

    deploy to gh-pages

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Covid Center Bot', // Title for your website.
   tagline: 'A website Documentation',
   url: 'http://imamaris.github.io', // Your website URL
-  baseUrl: '/covidcenter/', // Base URL for your project */
+  baseUrl: '/covidcenter.github.io/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
